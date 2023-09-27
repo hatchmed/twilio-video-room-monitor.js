880ba47f3a9032f11e9f39557c99c884389fe38d
Phillip Mrzyglocki <pmrzyg.upwork@gmail.com>
2023-09-27 10:18:49 -0400

    update packages

--- src/hooks/useIntervalUpdate/useIntervalUpdate.tsx
+++ src/hooks/useIntervalUpdate/useIntervalUpdate.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import React, { useState, useEffect, useRef } from 'react';
 import { UPDATE_INTERVAL } from '../../constants';
 
